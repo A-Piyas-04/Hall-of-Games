a4693f5abf2d08130c57f28aebc5fa96f69f383a
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2025-04-29 12:55:41 +0600

    played a little

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,7 +1,9 @@
 function Messege (){
-    let messege = [<h1>Hell oWo rld !</h1>];
-    let messege2 = ["Hello","World","!"];
-    let messege3 = ["Hello","World","!"];
+   let messege = [<h1>Hell oWo rld !</h1>];
+   let messege2 = [<h2>Hell oWo rld !</h2>];
+   let messege3 = [<h3>Hell oWo rld !</h3>];
 return messege;
+return messege2;
+return messege3;
 }
 export default Messege;
