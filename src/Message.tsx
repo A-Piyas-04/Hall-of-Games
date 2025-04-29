8ffe5037c4c5736ecf933a4896ff126011154fa4
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2025-04-29 12:52:35 +0600

    created first component

---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+function Messege (){
+    let messege = [<h1>Hell oWo rld !</h1>];
+    let messege2 = ["Hello","World","!"];
+    let messege3 = ["Hello","World","!"];
+return messege;
+}
+export default Messege;
