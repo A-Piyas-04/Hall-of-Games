e86b02d3989ca8059afc9503a5b6bb0087e76ba6
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2025-05-01 23:12:44 +0600

    correct game details and update genre format

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,15 @@ function App() {
       <GameCard game={{
         poster: "/images/RE2 Remake.jpg",
         title: "Resident Evil 2 Remake",
-        genre: "Horror Survival(Puzzle,Shooter)",
+        genre: "Horror Survival | (Puzzle,Shooter)",
         releaseYear: 2019,
       }} />
 
 <GameCard game={{
-        poster: "/images/mario.jpg",
-        title: "Resident Evil 2 Remake",
-        genre: "Horror Survival(Puzzle,Shooter)",
-        releaseYear: 2019,
+        poster: "/images/POP WW.jpg",
+        title: "Princess of Persia: Warrior Within",
+        genre: "Action Adventure",
+        releaseYear: 2004,
       }} />
 
     </>
