dc1779c2503106b4e1d1e04af467342967889845
A-Piyas-04 <ahnafpias@iut-dhaka.edu>
2025-05-03 01:10:35 +0600

    started wishlist logic

---
+++ src/pages/Wishlist.jsx
@@ -0,0 +1,11 @@
+function Wishlist() {
+
+    return (
+        <div className="wishlist">
+            <h2>Wishlist</h2>
+            <div className="wishlist-container">
+               <p className="wishlist-empty">Your wishlist is empty.</p>
+            </div>
+        </div>
+    )
+}
